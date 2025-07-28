80ec01b0b92d63028d9afd1f4977e5359c447e15
thifakr <thifakeren@gmail.com>
2025-07-28 08:07:32 +0700

    Add files via upload

--- react1.js
+++ react1.js
@@ -1,14 +1,10 @@
 let  sepeda = {
   name: "Polygon",
   gear: 24, 
-  weight: "100kg",
   color: "silver",
 
   start: function() {
-    console.log("Sepeda dinyalakan.");
-  },
-  drive: function() {
-    console.log("Sepeda berjalan.");
+     console.log("Sepeda berjalan.");
   },
   brake: function() {
     console.log("Sepeda mengerem.");
@@ -20,6 +16,6 @@ let  sepeda = {
  
 
 console.log(sepeda.name);
-console.log(sepeda.color); 
+console.log(sepeda.gear); 
 sepeda.start();
-sepeda.drive();
+sepeda.stop();
